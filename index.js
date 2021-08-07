e1cdf52562388e712a1753a2b42e551b60854a83
SpicyLunchbox <westonwoodard28@gmail.com>
2021-08-06 17:02:53 -0700

    finished second lesson

--- index.js
+++ index.js
@@ -14,5 +14,24 @@ talkFunction() // undefined
 let boundFunction = talkFunction.bind(dog)
 boundFunction() // "woof"
 
-let button = document.getElementById('myNiceButton')
-button.addEventListener('click', dog.talk.bind(dog))
+// let button = document.getElementById('myNiceButton')
+// button.addEventListener('click', dog.talk.bind(dog))
+
+// more exammples of bind and this
+function speak() {
+    console.log(this.sound)
+}
+
+let boromir = {
+    say: speak,
+    sound: 'one does not simply walk into mordor'
+}
+boromir.say()  // "one does not simply walk into mordor"
+let blabber = boromir.say
+blabber() // undefined
+
+let gollum = {
+    jabber: boromir.say,
+    sound: 'My precious'
+}
+gollum.jabber() // "My precious"
