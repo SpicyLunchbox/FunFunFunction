1a79dc5354af11b63d724e399bfdf53d80335923
SpicyLunchbox <westonwoodard28@gmail.com>
2021-08-09 17:32:03 -0700

    prototype lesson complete

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
 // Object Creation in Javascript
 
-// bind and this
-let dog = {
-    sound: 'woof',
-    talk: function() {
-        console.log(this.sound)
-    }
-}
+// Day 1) bind and this
+// let dog = {
+//     sound: 'woof',
+//     talk: function() {
+//         console.log(this.sound)
+//     }
+// }
 
-dog.talk() // "woof"
-let talkFunction = dog.talk
-talkFunction() // undefined
-let boundFunction = talkFunction.bind(dog)
-boundFunction() // "woof"
+// dog.talk() // "woof"
+// let talkFunction = dog.talk
+// talkFunction() // undefined
+// let boundFunction = talkFunction.bind(dog)
+// boundFunction() // "woof"
 
 // let button = document.getElementById('myNiceButton')
 // button.addEventListener('click', dog.talk.bind(dog))
 
-// more exammples of bind and this
-function speak() {
+// Day 2) more exammples of bind and this
+// function speak() {
+//     console.log(this.sound)
+// }
+
+// let boromir = {
+//     say: speak,
+//     sound: 'one does not simply walk into mordor'
+// }
+// boromir.say()  // "one does not simply walk into mordor"
+// let blabber = boromir.say
+// blabber() // undefined
+
+// let gollum = {
+//     jabber: boromir.say,
+//     sound: 'My precious'
+// }
+// gollum.jabber() // "My precious"
+
+
+// Day 3) Prototype Basics
+
+function talk() {
     console.log(this.sound)
 }
-
-let boromir = {
-    say: speak,
-    sound: 'one does not simply walk into mordor'
+let animal = {
+    talk
 }
-boromir.say()  // "one does not simply walk into mordor"
-let blabber = boromir.say
-blabber() // undefined
-
-let gollum = {
-    jabber: boromir.say,
-    sound: 'My precious'
+let dog = {
+    sound: 'woof!'
 }
-gollum.jabber() // "My precious"
+let prarieDog = {
+    howl: function() {
+        console.log(this.sound.toUpperCase())
+    }
+}
+Object.setPrototypeOf(dog, animal)
+animal.talk = function() {
+    console.log('i am a little teapot')
+}
+dog.talk()
+Object.setPrototypeOf(prarieDog, dog)
+prarieDog.howl()
