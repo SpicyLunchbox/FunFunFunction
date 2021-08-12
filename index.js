5751d3be3dd032fa6f376d1007d5568444890e9f
SpicyLunchbox <westonwoodard28@gmail.com>
2021-08-11 18:53:05 -0700

    proto vs prototype lesson completed

--- index.js
+++ index.js
@@ -63,32 +63,55 @@
 
 // Day 4) The new keyword
 
-function Person(saying) {
-    this.saying = saying
-}
+// function Person(saying) {
+//     this.saying = saying
+// }
 
-Person.prototype.talk = function() {
-    console.log('I say:', this.saying)
-}
+// Person.prototype.talk = function() {
+//     console.log('I say:', this.saying)
+// }
 
-var crockford = new Person('Semicolons!!!!!')
-crockford.talk()
+// var crockford = new Person('Semicolons!!!!!')
+// crockford.talk()
 
-// lets recreate new and what it does
-// remember that whenever a function object is created in javascript, it has the prototype property automatically
+// // lets recreate new and what it does
+// // remember that whenever a function object is created in javascript, it has the prototype property automatically
 
-function spawn(constructor) {
-    var obj = {}
-    Object.setPrototypeOf(obj, constructor.prototype)
-    var argsArray = Array.prototype.slice.apply(arguments)
-    constructor.apply(obj, argsArray.slice(1))
-    return obj
-}
-// this function shows what new is already doing
-// Array.from creates an array from whatever is passed in, in this case, the arguments property.  If you don't remember what that is,
-// console.log(arguments) or check out the 'new' keyword video at 17 min in
+// function spawn(constructor) {
+//     var obj = {}
+//     Object.setPrototypeOf(obj, constructor.prototype)
+//     var argsArray = Array.prototype.slice.apply(arguments)
+//     constructor.apply(obj, argsArray.slice(1))
+//     return obj
+// }
+// // this function shows what new is already doing
+// // Array.from creates an array from whatever is passed in, in this case, the arguments property.  If you don't remember what that is,
+// // console.log(arguments) or check out the 'new' keyword video at 17 min in
 
 
 
-var crockfard = spawn(Person, 'Semicolons!!!')
-crockfard.talk()
+// var crockfard = spawn(Person, 'Semicolons!!!')
+// crockfard.talk()
+
+// Day 5) __proto__ vs prototype
+
+// prototypes are just objects that delegate to other objects
+
+let cat = {breed: 'munchkin'}
+
+let myCat = {name: 'Fluffykins'}
+
+Object.setPrototypeOf(myCat, cat)
+
+cat.tailLength = 15
+
+console.log(myCat.name)
+
+console.log(myCat.breed)
+
+console.log(myCat.__proto__)
+
+// as you can see in the console, the __proto__ property on myCat is a reference to the prototype and any changes to the prototype will be reflected within __proto__
+// Remember that prototype is a property that is created on functions to allow for the new keyword to instantiate a new object based off the constructor function
+
+// to reiterate, __proto__ is a property on a normal object, prototype is a property of a constructor function
